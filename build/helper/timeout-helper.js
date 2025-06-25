dec657cf1e25affbfd0d6f1e6750095b5ba7b3ad
Jorge Arone <aronejorgesaul@gmail.com>
2025-06-25 14:17:30 -0500

    Add .gitignore and remove unnecessary files

--- build/helper/timeout-helper.js
+++
@@ -1,8 +0,0 @@
-export function createTimeoutSignal(timeoutMs) {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), timeoutMs);
-    return {
-        signal: controller.signal,
-        cleanup: () => clearTimeout(timeoutId)
-    };
-}
